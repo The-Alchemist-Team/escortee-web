fb4d9f5d87d9a3a3832b4e90c7a8d00732aaec5e
aslamcodes <mohamedaslammn.2003@gmail.com>
2022-03-10 12:11:07 +0530

    Firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9CSA0brrlBsFVH3b1d5pMME28Z6wzsOY",
+  authDomain: "escortee-ff721.firebaseapp.com",
+  projectId: "escortee-ff721",
+  storageBucket: "escortee-ff721.appspot.com",
+  messagingSenderId: "1016502809821",
+  appId: "1:1016502809821:web:6bfc001d9117e1ba985734",
+  measurementId: "G-3KLQHYG0J7",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
